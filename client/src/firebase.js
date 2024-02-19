0323c4ced9f90ef20e23a92241fce5acf28fa170
irannagkk <hejjegariranna@gmail.com>
2024-02-19 22:21:23 +0530

    added the mern real estate project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-real-estate-1a44a.firebaseapp.com",
+  projectId: "mern-real-estate-1a44a",
+  storageBucket: "mern-real-estate-1a44a.appspot.com",
+  messagingSenderId: "166161425652",
+  appId: "1:166161425652:web:3e64895f60ca0b3cbc6aa1"
 };
 
 // Initialize Firebase
