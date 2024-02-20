6bb16e9480510f2e1a4d06ead1204b0827010736
irannagkk <hejjegariranna@gmail.com>
2024-02-20 11:44:10 +0530

    modied all

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,36 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-real-estate-1a44a.firebaseapp.com",
-  projectId: "mern-real-estate-1a44a",
-  storageBucket: "mern-real-estate-1a44a.appspot.com",
-  messagingSenderId: "166161425652",
-  appId: "1:166161425652:web:3e64895f60ca0b3cbc6aa1"
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-real-estate-a4ce5.firebaseapp.com",
+//   projectId: "mern-real-estate-a4ce5",
+//   storageBucket: "mern-real-estate-a4ce5.appspot.com",
+//   messagingSenderId: "842863101365",
+//   appId: "1:842863101365:web:6452b066b174056ba9c5fc"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
